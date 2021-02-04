39a1a704441004e691f3d0b882b93deda77ed2e5
Gál Péter <pepyta118@gmail.com>
2021-02-04 19:17:09 +0100

    Publish built version

--- build/fetch/fetcher.js
+++ build/fetch/fetcher.js
@@ -1,4 +1,15 @@
 "use strict";
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -38,10 +49,25 @@ var __generator = (this && this.__generator) || function (thisArg, body) {
 Object.defineProperty(exports, "__esModule", { value: true });
 var API_ENDPOINT = process.env.NODE_ENV === "production" ? "https://ghoost.dev" : "http://localhost:3000";
 var fetcher = function (input, init) { return __awaiter(void 0, void 0, void 0, function () {
-    var res, data;
+    var parameters, i, key, data_1, res, data;
     return __generator(this, function (_a) {
         switch (_a.label) {
-            case 0: return [4, fetch(API_ENDPOINT + "/api" + input, init)];
+            case 0:
+                parameters = [];
+                if (window === null || window === void 0 ? void 0 : window.localStorage) {
+                    for (i = 0; i < localStorage.length; i++) {
+                        key = localStorage.key(i);
+                        if (!key)
+                            continue;
+                        data_1 = localStorage.getItem(key);
+                        if (!data_1)
+                            continue;
+                        parameters.push({ name: key, value: data_1 });
+                    }
+                    ;
+                }
+                ;
+                return [4, fetch(API_ENDPOINT + "/api" + input, parameters.length > 0 ? __assign(__assign({}, init), { headers: __assign(__assign({}, init === null || init === void 0 ? void 0 : init.headers), { cookie: join(parameters) }) }) : init)];
             case 1:
                 res = _a.sent();
                 if (!res.ok)
@@ -55,5 +81,8 @@ var fetcher = function (input, init) { return __awaiter(void 0, void 0, void 0, 
         }
     });
 }); };
+var join = function (parameters) {
+    return parameters.map(function (el) { return el.name + "=" + el.value; }).join("; ");
+};
 exports.default = fetcher;
 //# sourceMappingURL=fetcher.js.map
