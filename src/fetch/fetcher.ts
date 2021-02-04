c62e309e22db5da96c2672f7ee0ed5158e399d93
Gál Péter <pepyta118@gmail.com>
2021-02-04 19:11:32 +0100

    Load from localStorage if keys presist

--- src/fetch/fetcher.ts
+++ src/fetch/fetcher.ts
@@ -6,13 +6,37 @@ type ResponseType = {
 };
 
 const fetcher = async (input: RequestInfo, init?: RequestInit): Promise<ResponseType> => {
-    const res = await fetch(`${API_ENDPOINT}/api${input}`, init);
+    const parameters = [];
+
+    if(window?.localStorage) {
+        for(let i = 0; i < localStorage.length; i++) {
+            const key = localStorage.key(i);
+            if(!key) continue;
+
+            const data = localStorage.getItem(key);
+            if(!data) continue;
+
+            parameters.push({ name: key, value: data });
+        };
+    };
+    
+    const res = await fetch(`${API_ENDPOINT}/api${input}`, parameters.length > 0 ? {
+        ...init,
+        headers: {
+            ...init?.headers,
+            cookie: join(parameters),
+        },
+    } : init);
     if(!res.ok) throw new Error("Ghoost's servers are down or you don't have internet connection. Try again later!");
 
     const data = await res.json();
     if (data.error) throw new Error(data?.message);
 
     return data;
+};
+
+const join = (parameters: ({ name: string, value: string })[]) => {
+    return parameters.map((el) => `${el.name}=${el.value}`).join("; ");
 };
 
 export default fetcher;
