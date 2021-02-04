361d48671889bbf5a9dc3c45d83ee6b8280e6695
Gál Péter <pepyta118@gmail.com>
2021-02-04 21:14:30 +0100

    Add NextAuth as header parameter

--- src/fetch/fetcher.ts
+++ src/fetch/fetcher.ts
@@ -8,37 +8,26 @@ type ResponseType = {
 const fetcher = async (input: RequestInfo, init?: RequestInit): Promise<ResponseType> => {
     const parameters = [];
 
-    console.log(window.localStorage);
     if(window?.localStorage) {
         console.log(localStorage.length)
         for(let i = 0; i < localStorage.length; i++) {
             const key = localStorage.key(i);
-            console.log(key);
             if(!key) continue;
         
             const data = localStorage.getItem(key);
-            console.log(data);
             if(!data) continue;
 
             parameters.push({ name: key, value: data });
             console.log(parameters);
         };
     };
-
-    console.log(parameters);
-    console.log(parameters.length > 0 ? {
-        ...init,
-        headers: {
-            ...init?.headers,
-            cookie: join(parameters),
-        },
-    } : init);
     
     const res = await fetch(`${API_ENDPOINT}/api${input}`, parameters.length > 0 ? {
         ...init,
+        credentials: "include",
         headers: {
             ...init?.headers,
-            cookie: join(parameters),
+            NextAuth: join(parameters),
         },
     } : init);
     if(!res.ok) throw new Error("Ghoost's servers are down or you don't have internet connection. Try again later!");
